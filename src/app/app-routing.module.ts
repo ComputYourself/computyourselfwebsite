7df489efc4446d1288636be39f487597b69dd021
Sky <fabien-perot@orange.fr>
2020-08-01 11:31:40 +0200

    update app.component html css
    add navbar footer and update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,8 @@ import { EventComponent } from './event/event.component';
 import { HomeComponent } from './home/home.component';
 import { PictureComponent } from './picture/picture.component';
 import { UsefulInfoComponent } from './useful-info/useful-info.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FooterComponent } from './footer/footer.component';
 
 const routes: Routes = [
   { path: 'about', component: AboutComponent},
@@ -13,6 +15,8 @@ const routes: Routes = [
   { path: 'home', component: HomeComponent},
   { path: 'picture', component: PictureComponent},
   { path: 'info', component: UsefulInfoComponent},
+  { path: 'navbar', component: NavBarComponent},
+  { path: 'footer', component: FooterComponent},
   { path: '', component: HomeComponent, outlet: "hometest"},
   //{ path: '**', redirectTo: 'home'}
 
